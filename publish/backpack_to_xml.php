74cf24cf1012fb15cc6166a7ab0f5606439d49f3
sikelianos.com <sikelianos.com@a97380db-6046-0410-97f3-938e7e8e5c6c>
2008-04-06 19:14:31 +0000

    Workign on Backpack integration
    git-svn-id: svn+ssh://sikelianos.com/home/1939/data/svn/portfolio/trunk@15 a97380db-6046-0410-97f3-938e7e8e5c6c

--- publish/backpack_to_xml.php
+++ publish/backpack_to_xml.php
@@ -1,6 +1,6 @@
 <?php
 	include('SimpleBackpack.php');
-	$bp = new SimpleBackpack('sikelianos', 'ereiamJH');
+	$bp = new SimpleBackpack('sikelianos', 'aac9eee6defd8d18121a85a17fece3c6feb7aa69');
 	$result = $bp->export_backpack();
 	echo $result;
 ?>
