4e47f2de2d751e897680a000d2eacd8f0f2d8afd
sikelianos.com <sikelianos.com@a97380db-6046-0410-97f3-938e7e8e5c6c>
2008-04-06 20:28:18 +0000

    Working PHP script that CURLs XML from Backpack. All other PHP/Python stuff removed.
    git-svn-id: svn+ssh://sikelianos.com/home/1939/data/svn/portfolio/trunk@16 a97380db-6046-0410-97f3-938e7e8e5c6c

--- publish/backpack_to_xml.php
+++ publish/backpack_to_xml.php
@@ -1,6 +1,23 @@
 <?php
-	include('SimpleBackpack.php');
-	$bp = new SimpleBackpack('sikelianos', 'aac9eee6defd8d18121a85a17fece3c6feb7aa69');
-	$result = $bp->export_backpack();
-	echo $result;
+
+$username = "sikelianos";
+$token = "aac9eee6defd8d18121a85a17fece3c6feb7aa69";
+$page = "1054295";
+
+function curl_request($url, $request_body) {
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $request_body);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-POST_DATA_FORMAT: xml'));
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	$response = curl_exec($ch);	
+	curl_close($ch);
+	return $response;
+}
+
+$url = "http://".$username.".backpackit.com/ws/page/".$page;
+$request_body = "<request>\r\n<token>".$token."</token>\r\n</request>";
+$result = curl_request($url, $request_body);
+print_r($result);
+
 ?>
